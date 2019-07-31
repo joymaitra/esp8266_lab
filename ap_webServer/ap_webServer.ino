4e6d2baf3c3c4059573cd9e828d9e86d53ab8192
joymaitra <41389493+joymaitra@users.noreply.github.com>
2019-07-31 18:54:33 +0530

    Update ap_webServer.ino

--- ap_webServer/ap_webServer.ino
+++ ap_webServer/ap_webServer.ino
@@ -5,13 +5,19 @@
 const char *ssid = "ESP8266 Access Point"; // The name of the Wi-Fi network that will be created
 const char *password = "thereisnospoon";   // The password required to connect to it, leave blank for an open network
 
+IPAddress ip(192,168,11,4);
+IPAddress gateway(192,168,11,1);
+IPAddress subnet(255,255,255,0);
+
 void setup() {
   Serial.begin(115200);
   delay(10);
   Serial.println('\n');
   SPIFFS.begin();
 
+  WiFi.softAPConfig(ip, gateway, subnet);
   WiFi.softAP(ssid, password);             // Start the access point
+  
   Serial.print("Access Point \"");
   Serial.print(ssid);
   Serial.println("\" started");
